7c33a0f6e4b3bbf4062e74e62e50cd251eb028ac
siddhidoiphode <siddhidoiphode2022@gmail.com>
2025-09-10 22:04:47 +0530

    mm

---
+++ top100/Array/Symmetric pair array .py
@@ -0,0 +1,12 @@
+
+import numpy as np 
+from collections import Counter
+ans=[]
+arr=np.array([(10, 20), (30, 40), (20, 10), (50, 60), (40, 30), (60, 50), (70, 80), (80, 70), (90, 100)])
+# print(arr arr[2][::-1] in arr)
+# ans=[arr[i] if arr[i][::-1] in arr]
+for i in range(len(arr)):
+    if arr[i][::-1] in arr:
+        ans.append(arr[i])
+
+print(ans)
